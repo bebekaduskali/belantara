3430afcefba03d67596922570bf44e460e8d285b
bebekaduskali <erwin.bwi@gmail.com>
2017-10-30 11:36:01 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,288 @@
+<?php include'head.php'?>
+
+        <div id="slider" class="inspiro-slider arrows-large arrows-creative dots-creative" data-height-xs="360">
+
+            <!-- Slide 1 -->
+            <div class="slide kenburns" style="background-image:url('images/slider/gajah.jpg');">
+                <div class="container">
+                    <div class="slide-captions">
+                        <!-- Captions -->
+                        <h1 data-caption-animation="zoom-out">Conserve <br> Indonesia's<br>Natural</br>Environment</h1>
+                        <!-- end: Captions -->
+                    </div>
+                </div>
+            </div>
+            <!-- end: Slide 1 -->
+
+            <!-- Slide 2 -->
+            <div class="slide background-overlay-one kenburns" style="background-image:url('images/slider/monyet.jpg');">
+                <div class="container">
+                    <div class="slide-captions">
+                        <!-- Captions -->
+                        <h1 data-caption-animation="zoom-out">Conserve <br> Indonesia's<br>Natural</br>Environment</h1>
+                        <!-- end: Captions -->
+                    </div>
+                </div>
+            </div>
+            <!-- end: Slide 2 -->
+
+        </div>
+        
+        <section class="background-overlay-grey  p-t-100 p-b-100" data-velocity="-.30" data-parallax-image="images/parallax/04.jpg">
+            <div class="container xs-text-center sm-text-center text-light">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 text-center"> 
+                    <h1 class="text-white">Dare to Share Your Idea To Us ? <br>Then show it !!!</h1>
+                    <p class="text-white">That is creativity </p>
+                    <a class="btn btn-success" href="#">
+                        I Need More Info
+                    </a>
+                    <a class="btn btn-warning" href="#">
+                      Register
+                   </a>
+                  </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <div class="row">
+                  <div class="col-lg-6 col-md-6">
+                      <h2 >Get to know us better.</h2>
+                      <p class="m-b-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quod conseqt quibusdam, enim expedita sed quia nesciunt incidunt accusamus necessitatibus modi adipisci officia libero accusantium esse hic, obcaecati, ullam, laboriosam!</p>
+                      <p class="m-b-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti vero, animi suscipit id facere officia. Aspernatur, quo, quos nisi dolorum aperiam fugiat deserunt velit rerum laudantium cum magnam.</p>
+                      <ul class="list-icon list-icon-check list-icon-colored m-b-10">
+                        <li class="lihome">Nulla vehicula nisl cum soluta nobis est eligendi optio cumque </li>
+                        <li class="lihome">nihil impeditnon enim fermentum veniamDuis nec aliquet massa</li>
+                        <li class="lihome">sed dapibus mauris. Nulla vehicula nisl cum soluta nobis est eligendi</li>
+                      </ul> 
+                      <p>Dolor sit amet, consectetur adipisicing elit. Vero quod conseqt quibusdam, enim expedita sed quia nesciunt incidunt accusamus necessitatibus.</p>
+                  </div>  
+                  <div class="col-lg-6 col-md-6 col-sm-6">
+                    <video class="text-center" width="100%" height="100%" controls="" poster="video/cover.png">
+                        <source src="video/home.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                  </div>
+                </div>
+            </div>
+            <div class="call-to-action background-grey m-b-0">
+                <div class="container">
+                    <div class="col-md-6 col-lg-9">
+                        <h3>We Have More Stories For You to Know</h3>
+                        <p>Belantara Foundation believes that a sustainable landscape approach must incorporate a harmony of principles that not only protect the livelihoods of the people, particularly at a grassroots level, but also see to a value added protection of natural resources and biodiversity long into the future.</p>
+                    </div>
+                    <div class="col-md-6 col-lg-3 text-center "> <a class="btn btn-lg " href="aboutus.php">About Us</a> </div>
+                </div>
+            </div>
+            <div class="container">
+            <div class="p-t-80">
+                <div class="row">
+                    <div class="col-md-6 col-lg-8">
+                        <img class="img-responsive" src="images/about/infograph.jpg"> 
+                    </div>
+                    <div class="col-md-6 col-lg-4">
+                        <h2>MULTISTAKHOLDER APPROACH : <span class="text-green">CONNECTING THE PIECES</span></h2>
+                        <hr>
+                        <p>Belantara align conservation activities carried out by multiple stakeholders and ensure these initiatives support an environmentally, socially, culturally, and economically sustainable landscape.</p>
+                    
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+        
+        <section class="background-orange"><div class="container">
+            <div class="row text-light">
+              <div class="col-md-4">
+                <div class="icon-box effect medium border center">
+                  <div class="icon"> <a href="#"><i class="fa fa-heart"></i></a> </div>
+                  <div class="counter bold"> <span data-speed="3500" data-refresh-interval="50" data-to="88000" data-from="0" data-seperator="true">88000</span> </div>
+                  <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="icon-box effect medium border center">
+                  <div class="icon"> <a href="#"><i class="fa fa-lightbulb-o"></i></a> </div>
+                  <div class="counter"> <span data-speed="1500" data-refresh-interval="50" data-to="4500" data-from="1000" data-seperator="true">4500</span> </div>
+                  <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="icon-box effect medium border center">
+                  <div class="icon"> <a href="#"><i class="fa fa-child"></i></a> </div>
+                  <div class="counter bold"> <span data-speed="3500" data-refresh-interval="50" data-to="88000" data-from="0" data-seperator="true">88000</span> </div>
+                  <p>Lorem ipsum dolor sit amet, consecte adipiscing elit.</p>
+                </div>
+              </div>
+            </div>
+        </div></section>
+        
+        <section class="p-t-50 background-1">
+            <div class="container">
+                <h3 class="text-center">GRANT DISTRIBUTION MAP</h3>
+                <img class="img-responsive animated fadeInUpBig visible" data-animation="fadeInUpBig" src="images/about/peta.png">
+            </div>
+        </section>
+        
+        <section class="background-overlay-one p-t-50 p-b-50" data-velocity="-.10" data-parallax-image="images/parallax/parallax.png">
+            <div class="container">
+                <div class="heading heading-center text-light">
+                    <h2>OUR BLOG</h2>
+                    <span class="lead font-weight-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla luctus. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. </span>
+                </div>
+                <div id="blog" class="grid-layout post-3-columns grid-loaded" data-item="post-item" style="margin: 0px -20px -20px 0px; position: relative; height: 375px;">
+
+                    <!-- Post item-->
+                    <div class="post-item" style="padding: 0px 20px 20px 0px; position: absolute; left: 0px; top: 0px;">
+                        <div class="post-item-wrap background-transparent">
+                            <div class="post-image">
+                                <a href="#">
+                                    <img alt="" src="images/upload/1.jpg">
+                                </a>
+                            </div>
+                            <div class="post-item-description text-light">
+                                <h2><a href="#">Lighthouse, standard post with a single image</a></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end: Post item-->
+
+                    <!-- Post item-->
+                    <div class="post-item" style="padding: 0px 20px 20px 0px; position: absolute; left: 386px; top: 0px;">
+                        <div class="post-item-wrap background-transparent">
+                            <div class="post-image">
+                                <a href="#">
+                                    <img alt="" src="images/upload/2.jpg">
+                                </a>
+                            </div>
+                            <div class="post-item-description text-light">
+                                <h2><a href="#">Lighthouse, standard post with a single image</a></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end: Post item-->
+
+
+                    <!-- Post item-->
+                    <div class="post-item" style="padding: 0px 20px 20px 0px; position: absolute; left: 773px; top: 0px;">
+                        <div class="post-item-wrap background-transparent">
+                            <div class="post-image">
+                                <a href="#">
+                                    <img alt="" src="images/upload/3.jpg">
+                                </a>
+                            </div>
+                            <div class="post-item-description text-light">
+                                <h2><a href="#">Lighthouse, standard post with a single image</a></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end: Post item-->
+                </div>
+            </div>
+        </section>
+
+        <section class="background-dark text-center p-t-80  p-b-30">
+            <div class="container">
+                <div class="widget clearfix widget-newsletter">
+                    <form class="widget-subscribe-form form-inline" action="include/subscribe-form.php" role="form" method="post" novalidate="novalidate">
+                        <h3 class="text-light">Subscribe to our Newsletter</h3>
+
+                        <div class="input-group" style="width:70%;">
+                            <span class="input-group-addon background-green"><i class="fa fa-paper-plane"></i></span>
+                            <input type="email" aria-required="true" name="widget-subscribe-form-email" class="form-control required email" placeholder="Enter your Email">
+                            <span class="input-group-btn">
+											<button type="submit" class="btn">Subscribe</button>
+										</span>
+                        </div>
+                        <small class="text-light">Stay informed on our latest news!</small>
+                    </form>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="widget">
+                            <h3>Latest Resource</h3>
+                            <div class="post-thumbnail-list">
+                                <div class="post-thumbnail-entry">
+                                    <img alt="" src="images/upload/3.jpg">
+                                    <div class="post-thumbnail-content">
+                                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+                                    </div>
+                                </div>
+                                <div class="post-thumbnail-entry">
+                                    <img alt="" src="images/upload/2_thumb.jpg">
+                                    <div class="post-thumbnail-content">
+                                        <a href="#">Fringilla luctus Lorem ipsum dolor sit amet</a>
+                                    </div>
+                                </div>
+                                <div class="post-thumbnail-entry">
+                                    <img alt="" src="images/upload/1_thumb.jpeg">
+                                    <div class="post-thumbnail-content">
+                                        <a href="#">Consectetur adipiscing elit</a>
+                                    </div>
+                                </div>
+                            </div>
+                         </div>
+                    </div>
+                
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <h3>Instagram</h3>
+                        <div class="widget widget-gallery">
+
+                            <div data-lightbox="gallery">
+                                <a href="homepages/magazine/images/news/grid/5.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/5.jpg">
+                                </a>
+                                <a href="homepages/magazine/images/news/grid/6.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/6.jpg">
+                                </a>
+                                <a href="homepages/magazine/images/news/grid/7.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/7.jpg">
+                                </a>
+                                <a href="homepages/magazine/images/news/grid/8.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/8.jpg">
+                                </a>
+                                <a href="homepages/magazine/images/news/grid/9.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/9.jpg">
+                                </a>
+                                <a href="homepages/magazine/images/news/grid/10.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/10.jpg">
+                                </a>
+                                <a href="homepages/magazine/images/news/grid/11.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/11.jpg">
+                                </a>
+                                <a href="homepages/magazine/images/news/grid/12.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/12.jpg">
+                                </a>
+                                <a href="homepages/magazine/images/news/grid/13.jpg" data-lightbox="gallery-item">
+                                    <img alt="image" src="homepages/magazine/images/news/grid/13.jpg">
+                                </a>
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <div class="grid-item">
+                        <!-- Twitter widget -->
+                        <div class="widget widget-tweeter" class="widget-tweeter" data-username="envato" data-limit="2">
+                            <h3>Recent Tweets</h3>
+                        </div>
+                        <!-- end: Twitter widget-->
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+<?php include'footer.php'?>
